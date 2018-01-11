e0236599bfaa754dd226e0529916b0ac691f74fd
ys <123@qq.com>
2018-01-11 20:17:17 +0800

    初始提交

---
+++ src/main/java/debugger/MycatStartupTest.java
@@ -0,0 +1,13 @@
+package debugger;
+
+import io.mycat.MycatStartup;
+
+/**
+ * Created by Ytadpole on 2018/1/11.
+ */
+public class MycatStartupTest {
+
+    public static void main(String[] args){
+        MycatStartup.main(args);
+    }
+}
